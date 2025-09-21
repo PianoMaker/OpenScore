a26a202f5d78f3deb194e384b591cc2acf74f88b
Andriy Bondarenko <bondareandre@gmail.com>
2025-09-21 22:01:01 +0300

    Change OnGet method to return IActionResult
    Updated the OnGet method to return an IActionResult instead of void. Added a redirect to the "/OsmdDemo" page.

--- OpenScore/Pages/Index.cshtml.cs
+++ OpenScore/Pages/Index.cshtml.cs
@@ -12,9 +12,9 @@ namespace OpenScore.Pages
             _logger = logger;
         }
 
-        public void OnGet()
+        public IActionResult OnGet()
         {
-
+            return RedirectToPage("/OsmdDemo");
         }
     }
 }
